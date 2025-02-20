13bef3554398a7382fed04c2b01330a16ef1b774
Peter Kerschbaumer <peter@kerschbaumer.se>
2025-02-20 17:59:34 +0100

    Refactored main.go and improved error handling

--- main.go
+++ main.go
@@ -11,61 +11,22 @@ import (
 	"github.com/Germanicus1/crizzcrozz/pkg/models"
 )
 
-// TODO-dNww-d: Refactor main.go
-
 //TODO-yUQLxC: Read words from file
 
 func main() {
-	var width int
-	height := 0
-	flag.IntVar(&width, "width", 23, "The width of the board")
-	flag.IntVar(&height, "height", height, "The width of the board")
-	wordList := flag.String("words", "", "A comma separated list of words")
-	flag.Parse()
+	width, height, wordList := parseFlags()
+	words := processWordList(wordList)
 
-	words := strings.Split(*wordList, ",")
-
-	for i, v := range words {
-		words[i] = strings.TrimSpace(v)
-	}
-
-	if height == 0 {
-		height = width
-	}
-	// Create the board bouindaries for the crosword puzzle
-	bounds, _ := models.NewBoundsRectangle(width, height)
-
-	// Create a word pool with some sample words
-
-	if len(words) == 0 {
-		words = []string{"bar", "beispiel", "bezahlen", "cent", "zusammen", "stimmt", "eingeladen", "essen", "euro", "gast", "kellner", "kellnerin", "rechnung", "sagen", "trinkgeld", "kosten", "viel", "zahlen", "karte", "getrennt", "zusammen"}
-	}
-
-	for i, word := range words {
-		words[i] = strings.ToUpper(word)
-	}
-
-	sort.Slice(words, func(j, i int) bool {
-		return len(words[i]) < len(words[j])
-	})
-
-	board := models.NewBoard(bounds, len(words))
-
-	// initialize a new pool of words.
-	newPool := models.NewPool()
-	newPool.LoadWords(words)
-
-	generator := generators.NewAsymmetricalGenerator(board, newPool)
-
-	// Generate the crossword
-	err := generator.Generate()
-	fmt.Println("wordcount:", board.WordCount)
+	board, err := setUpBoard(width, height, words)
 	if err != nil {
 		fmt.Println("Failed to generate the crossword:", err)
-		printBoard(board)
 		return
 	}
 
+	if err := generateCrossword(board, words); err != nil {
+		fmt.Println("Failed to generate the crossword:", err)
+		return
+	}
 	printBoard(board)
 }
 
@@ -81,4 +42,68 @@ func printBoard(b *models.Board) {
 		}
 		fmt.Println()
 	}
+}
+
+func parseFlags() (int, int, string) {
+	var width, height int
+	var wordList string
+	flag.IntVar(&width, "width", 23, "The width of the board")
+	flag.IntVar(&height, "height", 0, "The width of the board")
+	flag.StringVar(&wordList, "words", "", "A comma separated list of words")
+	flag.Parse()
+
+	if height == 0 {
+		height = width
+	}
+
+	return width, height, wordList
+}
+
+func processWordList(wordList string) []string {
+	var words []string
+	if wordList != "" {
+		words = strings.Split(wordList, ",")
+		for i, v := range words {
+			words[i] = strings.TrimSpace(v)
+		}
+	} else {
+		words = []string{"bar", "beispiel", "bezahlen", "cent", "zusammen", "stimmt", "eingeladen", "essen", "euro", "gast", "kellner", "kellnerin", "rechnung", "sagen", "trinkgeld", "kosten", "viel", "zahlen", "karte", "getrennt", "zusammen"}
+	}
+
+	for i, word := range words {
+		words[i] = strings.ToUpper(word)
+	}
+
+	sort.Slice(words, func(j, i int) bool {
+		return len(words[i]) < len(words[j])
+	})
+	return words
+}
+
+func setUpBoard(width, height int, words []string) (*models.Board, error) {
+	// Create the board bouindaries for the crossword puzzle
+	bounds, err := models.NewBoundsRectangle(width, height)
+	if err != nil {
+		return nil, err
+	}
+	board := models.NewBoard(bounds, len(words))
+	return board, nil
+}
+
+func generateCrossword(b *models.Board, words []string) error {
+	// initialize a new pool of words.
+	newPool := models.NewPool()
+	newPool.LoadWords(words)
+
+	generator := generators.NewAsymmetricalGenerator(b, newPool)
+
+	// Generate the crossword
+	err := generator.Generate()
+	// fmt.Println("wordcount:", b.WordCount)
+	if err != nil {
+		fmt.Println("Failed to generate the crossword:", err)
+		// printBoard(board)
+		return err
+	}
+	return nil
 }
